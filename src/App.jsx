5e530dbe85df7265feb8dbb49b9148a8ff696b0e
rohitsahu23866 <76580992+rohitsahu23866@users.noreply.github.com>
2024-07-15 15:30:07 +0530

    second

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@
 import React from 'react';
 import Calculator from './components/Calculator.jsx';
 import Footer from './components/Footer.jsx';
+import { TypeAnimation } from 'react-type-animation';
 
 const App = () => {
   return (
-    <div className="App min-h-screen flex flex-col items-center justify-center bg-gray-900 px-4">
+    <div className="App min-h-screen flex flex-col items-center justify-center bg-black px-4">
       <header className="App-header mb-6">
-        <h1 className="mt-10 text-4xl font-bold text-white sm:text-5xl md:text-6xl">Calculator</h1>
+        <h1 className="mt-10 text-4xl font-bold primary-color sm:text-5xl md:text-6xl">
+          <TypeAnimation sequence={["Calculator",5000,"",1000]} wrapper='span' speed={50} repeat={Infinity}
+          />
+          </h1>
       </header>
       <Calculator />
       <Footer />
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="w-[100%] mt-10 p-6 bg-gray-700 text-white text-center rounded-lg shadow-lg">
+    <footer className="w-[100%] mt-10 p-6 bg-primary-color text-black text-center rounded-lg shadow-lg">
       <p className="mb-2 text-lg font-bold">Calculator App</p>
       <p className="text-sm">
         This calculator app was developed as a part of my resume project. It showcases my skills in React and Tailwind CSS.
